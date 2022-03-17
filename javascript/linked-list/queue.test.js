7e2299b48e1497fa5b0b05204bce13ff3fdd9828
bushraAD98 <bushradawdyeh@gmaiil.com>
2022-03-17 18:53:10 +0300

    done

--- javascript/linked-list/queue.test.js
+++ javascript/linked-list/queue.test.js
@@ -20,5 +20,35 @@ describe("testing queue", () => {
 
   });
 
+  it("test deleting the first node in the queue", () => {
+    const queue = new Queue();
+    const q = new Queue();
+    queue.enqueue(3);
+    queue.enqueue(2);
+    queue.enqueue(1);
+expect(queue.dequeue()).toBe(3);
+expect(q.dequeue()).toBe('Exception!');
+  });
+
+  it("test returning the first node in the queue", () => {
+    const queue = new Queue();
+    const q = new Queue();
+    queue.enqueue(3);
+    queue.enqueue(2);
+    queue.enqueue(1);
+expect(queue.dequeue()).toBe(3);
+expect(q.dequeue()).toBe('Exception!');
+  });
+
+
+  
+it('tset isEmpty method',()=>{
+  const queue = new Queue();
+  const q = new Queue();
+  queue.enqueue(100);
+expect(q.isEmpty()).toBe(true);
+expect(queue.isEmpty()).toBe(false);
+
+})
 
 });
