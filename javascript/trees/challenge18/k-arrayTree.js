e59290047237bcf67feaded43f68d407a39ea66e
bushraAD98 <bushradawdyeh@gmaiil.com>
2022-05-29 21:09:15 +0300

    chal18-done

---
+++ javascript/trees/challenge18/k-arrayTree.js
@@ -0,0 +1,56 @@
+class Node {
+    constructor(value, left = null, right = null) {
+      this.value = value
+      this.left = left
+      this.right = right
+    }
+  }
+  
+  // Fizz Buzz
+  let fizzBuzz = (value) => {
+    if (value%15 === 0) {
+      value = 'FizzBuzz'
+    } else if (value%3 === 0) {
+      value = 'Fizz'
+    } else if (value%5 === 0) {
+      value = 'Buzz'
+    } else {
+      value = value.toString()
+    }
+    return value
+  }
+  
+  // traverse tree
+  let traverse = (node) => {
+    if (node === null) return null
+  
+    let newValue = fizzBuzz(node.value)
+  
+    let newNode = new Node(newValue)
+    newNode.left = traverse(node.left)
+    newNode.right = traverse(node.right)
+  
+    return newNode
+  }
+  
+  //Binary Tree class
+  class BinaryTree {
+    constructor() {
+      this.root = null;
+    }
+  
+    //Fizz Buzz method
+    fizzBuzzTree() {
+      let newTree = new BinaryTree
+  
+      //call traverse
+      newTree.root = traverse(this.root)
+      return newTree
+    }
+  
+  }
+  
+  module.exports = {
+    Node,
+    BinaryTree
+  }
