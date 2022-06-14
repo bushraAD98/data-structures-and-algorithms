aa067b3f6c2c6b733ab6c1075235be1368eac6b6
bushraAD98 <bushradawdyeh@gmaiil.com>
2022-06-14 17:44:17 +0300

    chal38-full solution

--- javascript/Graph/__tests__/graph.test.js
+++ javascript/Graph/__tests__/graph.test.js
@@ -114,32 +114,24 @@ describe("GRAPH TESTS", () => {
       graph.add_edge('Naboo', 'Narnia', 250);
       graph.add_edge('Monstropolis', 'Narnia', 37);
   
-      // expect(graph.businessTrip(graph, ['Pandora', 'Metroville'])).toBe(82);
-      // expect(graph.businessTrip(graph, ['Arendelle', 'Monstropolis', 'Naboo'])).toBe(115);
-      // expect(graph.businessTrip(graph, ['Pandora', 'Arendelle', 'Metroville', 'Naboo'])).toBe(275);
-      // expect(graph.businessTrip(graph, ['Pandora', 'Narnia'])).toBe(null);
+      expect(graph.businessTrip(graph, ['Pandora', 'Metroville'])).toBe(82);
+      expect(graph.businessTrip(graph, ['Arendelle', 'Monstropolis', 'Naboo'])).toBe(115);
+      expect(graph.businessTrip(graph, ['Pandora', 'Arendelle', 'Metroville', 'Naboo'])).toBe(275);
+      expect(graph.businessTrip(graph, ['Pandora', 'Narnia'])).toBe(null);
     });
 
 
     it('DEPTH-FIRST-TEST', ()=>{
-      let g = new Graph();
-
-      g.add_node("A")
-      g.add_node("B")
-      g.add_node("C")
-      g.add_node("D")
-      g.add_node("E")
-      g.add_node("F")
-      
-      
-      g.add_edge("A", "B")
-      g.add_edge("A", "C")
-      g.add_edge("B","D")
-      g.add_edge("C","E")
-      g.add_edge("D","E")
-      g.add_edge("D","F")
-      g.add_edge("E","F")
-      
-      expect(g.depthFirst('A')).toEqual(['A', 'B', 'D', 'C', 'E','F']);
+      const graph = new Graph();
+      graph. add_node('A');
+      graph. add_node('B');
+      graph. add_node('C');
+      graph. add_node('D');
+      graph. add_node('E');
+      graph.add_edge('A', 'B');
+      graph.add_edge('A', 'C');
+      graph.add_edge('B', 'D');
+      graph.add_edge('C', 'E');
+      expect(graph.depthFirst('A')).toEqual(['A', 'B', 'D', 'C', 'E']);
     });
   });
---
+++ javascript/Graph/chal38.readme.md
@@ -0,0 +1,14 @@
+
+# Depth First Traversal
+Depth First Search (DFS) algorithm traverses a graph in a depthward motion and uses a stack to remember to get the next vertex to start a search, when a dead end occurs in any iteration.
+
+## Challenge
+the challenge asked us to write a function called depth-first that takes a start point node to traverse from it to all the nodes then return A collection of nodes in their pre-order depth-first traversal order
+
+## Approach & Efficiency
+i used the recursion approach because its much efficient than iterative approach when dealing with depth first traverse 
+it will ends with O(n) for time & space complexity becaause it depends on how many nodes we are traversing at.
+
+## Solution
+
+![img](./chal38.PNG)
